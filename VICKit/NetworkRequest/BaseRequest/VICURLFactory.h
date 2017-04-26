cff8254de9bc5deae5c9bf4c8a885587fca40259
静花寒 <754612130@qq.com>
2017-04-26 14:04:26 +0800

    更新VICNetWork
    Signed-off-by: 静花寒 <754612130@qq.com>

---
+++ VICKit/NetworkRequest/BaseRequest/VICURLFactory.h
@@ -0,0 +1,25 @@
+//
+//  VICURLFactory.h
+//  CommonAPI
+//
+//  Created by qianyb on 15/4/10.
+//  Copyright (c) 2015年 qianyb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VICURLFactory : NSObject
+
+/**
+ *  返回URL地址
+ *
+ *  @return 服务端API的URL地址
+ */
++ (NSURL *)relativeBaseURL;
+/**
+ *  返回URL地址
+ *
+ *  @return 服务端上传文件的URL地址
+ */
++ (NSURL *)uploadRelativeBaseURL;
+@end
